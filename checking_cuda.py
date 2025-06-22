a5f154728969eab19676059333bfdcf9ad24d937
pminhtu24 <pminhtu24@gmail.com>
2025-06-22 16:11:50 +0700

    update

--- checking_cuda.py
+++ checking_cuda.py
@@ -1,4 +1,4 @@
 import torch
 print(torch.__version__)
-print(torch.cuda.get_arch_list())  # phải ra như ['sm_86', 'sm_89', ...] nếu GPU được hỗ trợ
+print(torch.cuda.get_arch_list())  # ['sm_86', 'sm_89', ...]
 print(torch.cuda.is_available())
